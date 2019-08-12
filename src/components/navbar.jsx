965b5634995faf7442d35656490c1ce5b6ee4b1d
Russell <russelldoe@hotmail.co.uk>
2019-08-12 21:09:07 +0100

    added logo

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import wclogo from '../img/wclogogradient2.png';
+
 const NavBar = () => (
     <div className="navBar">
         <div className="textLogo">
-            <Link className="title" to="/">Wigan Coders</Link>
+            <Link className="title" to="/"><img src={wclogo} alt="wclogo"/></Link>
               <ul className="nav">
                 <Link className="item" to="/about-us">About Us</Link>
                 <Link className="item" to="/contact-us">Contact Us</Link>
