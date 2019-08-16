955042b6f4d08a23c2429f0a0f7421cefeb26f36
Russell <russelldoe@hotmail.co.uk>
2019-08-16 15:05:25 +0100

    fixed navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import wclogo from '../img/wclogocoloursmall.png';
+import wclogo from '../img/wclogogradient2small.png';
 
 const NavBar = () => (
     <div className="navBar">
         <div className="textLogo">
-            <Link className="title" to="/"><img src={wclogo} alt="wclogo"/></Link>
+            <Link className="title" to="/"><img className="nav-logo" src={wclogo} alt="wclogo"/></Link>
+        </div>    
               <ul className="nav">
                 <Link className="item" to="/about-us">About Us</Link>
                 <Link className="item" to="/contact-us">Contact Us</Link>
               </ul>
-        </div>    
+           
     </div>
 );
 
