bfa38b28bffce201fc1b8fe455768a6dac3216a9
Russell <russelldoe@hotmail.co.uk>
2019-09-01 23:05:29 +0100

    setup programming foundations cards section

--- src/components/stack.jsx
+++ src/components/stack.jsx
@@ -5,21 +5,91 @@ import cssLogo from '../img/CSS-logo.jpg';
 import jsLogo from '../img/JS-logo.jpg';
 
 const Stack = () => (
-    <div className="tech-stack">
-      <div className="stack-heading">
-        <p>stack heading</p>
-      </div>
+   <div> 
+    <div class="u-center-text u-margin-bottom-big">
+        <h2 class="heading-secondary">
+            Programming Foundations
+        </h2>
+    </div>
 
-      <div className="fundamentals-image">  
-        <img src={ htmlLogo } alt="html-logo"/>
-        <img src={ cssLogo } alt="css-logo" />
-        <img src={ jsLogo } alt="js-logo"/>
-      </div>
-        
-      <div className="fundamentals-text">
-        <p>fundamentals text</p>
-      </div>
-    </div> 
+    <div class="row">
+        <div class="col-1-of-3">
+            <div class="card">
+                <div class="card__side card__side--front">
+                    <div class="card__picture card__picture--1">
+                        &nbsp;
+                    </div>
+                    <h4 class="card__heading">
+                        <span class="card__heading-span card__heading-span--1">HTML 5</span>
+                    </h4>
+                    <div class="card__details">
+                        <ul>
+                            <li>Lorem ipsum</li>
+                            <li>Lorem ipsum</li>
+                            <li>Lorem ipsum</li>
+                            <li>Lorem ipsum</li>
+                            <li>Lorem ipsum</li>
+                        </ul>
+                    </div>
+
+                </div>
+                <div class="card__side card__side--back card__side--back-1">
+                    BACK
+                </div>
+            </div>
+        </div>
+        <div class="col-1-of-3">
+            <div class="card">
+                <div class="card__side card__side--front">
+                    <div class="card__picture card__picture--2">
+                        &nbsp;
+                    </div>
+                    <h4 class="card__heading">
+                        <span class="card__heading-span card__heading-span--2">CSS 3</span>
+                    </h4>
+                    <div class="card__details">
+                        <ul>
+                            <li>Lorem ipsum</li>
+                            <li>Lorem ipsum</li>
+                            <li>Lorem ipsum</li>
+                            <li>Lorem ipsum</li>
+                            <li>Lorem ipsum</li>
+                        </ul>
+                    </div>
+
+                </div>
+                <div class="card__side card__side--back card__side--back-2">
+                    BACK
+                </div>
+            </div>
+        </div>
+        <div class="col-1-of-3">
+            <div class="card">
+                <div class="card__side card__side--front">
+                    <div class="card__picture card__picture--3">
+                        &nbsp;
+                    </div>
+                    <h4 class="card__heading">
+                        <span class="card__heading-span card__heading-span--3">JavaScript</span>
+                    </h4>
+                    <div class="card__details">
+                        <ul>
+                            <li>Lorem ipsum</li>
+                            <li>Lorem ipsum</li>
+                            <li>Lorem ipsum</li>
+                            <li>Lorem ipsum</li>
+                            <li>Lorem ipsum</li>
+                        </ul>
+                    </div>
+
+                </div>
+                <div class="card__side card__side--back card__side--back-3">
+                    BACK
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>            
 )
 
 export default Stack;
